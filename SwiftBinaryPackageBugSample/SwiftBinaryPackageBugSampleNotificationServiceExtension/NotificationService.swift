437c136c5616e845fc4fcdbe458f3f02264f7455
Karim H <karim@karhm.com>
2020-10-22 14:52:54 +0100

    Add `SwiftBinaryPackageBugSample` sample project

---
+++ SwiftBinaryPackageBugSample/SwiftBinaryPackageBugSampleNotificationServiceExtension/NotificationService.swift
@@ -0,0 +1,7 @@
+import UserNotifications
+
+class NotificationService: UNNotificationServiceExtension {
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        contentHandler(request.content)
+    }
+}
